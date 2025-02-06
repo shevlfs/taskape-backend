dd33f3e30346bc041f72e88e7c8df9170c6cb57c
Theodor Shevlyakov <shevlfs@icloud.com>
2025-02-06 21:44:38 +0300

    initial test server

---
+++ server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "taskape-server/grpc-example/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello " + req.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Println("Server started on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
